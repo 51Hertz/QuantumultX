e8c27cf06dbc9f985060e1a20369361a78867ff9
paidaxing <73331463+51Hertz@users.noreply.github.com>
2024-09-09 16:47:30 +0800

    Update bsklGetToken.js

--- bsklGetToken.js
+++ bsklGetToken.js
@@ -2,14 +2,13 @@
 let body = $response.body;
 let obj = JSON.parse(body); // 解析为JSON对象
 
-console.log("obj: " + obj);    
-
 // 提取token字段
 let token = obj.token;
 
 // 将提取的token保存到圈X环境变量
 if (token) {
   $prefs.setValueForKey(token, "wechat_token");  // 保存token
+  $clipboard.set(token);  // 保存到剪贴板
   console.log("Token saved: " + token);          // 打印日志
 } else {
   console.log("Token not found in response");     // 无法提取时的日志提示
